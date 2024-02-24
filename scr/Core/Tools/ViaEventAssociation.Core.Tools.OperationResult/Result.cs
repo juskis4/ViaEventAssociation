1072ecfb793d52fd1488cc70c5d4422558c03695
Lukas Juskevicius <ljuskevicius2002@gmail.com>
2024-02-24 15:43:45 +0100

    Update Result.cs

--- scr/Core/Tools/ViaEventAssociation.Core.Tools.OperationResult/Result.cs
+++ scr/Core/Tools/ViaEventAssociation.Core.Tools.OperationResult/Result.cs
@@ -1,19 +1,52 @@
 namespace ViaEventAssociation.Core.Tools.OperationResult;
 
-public class Result<T>
+public class Result
 {
-    public T Data { get; private set; } = default!;
-    public string ErrorMessage { get; private set; } = null!;
-    public Exception Exception { get; private set; } = null!;
-    
-    public bool IsSuccess => string.IsNullOrEmpty(ErrorMessage) && Exception == null;
+    private readonly List<string> _errors = new List<string>();
+    public IEnumerable<string> Errors => _errors.AsReadOnly();
+    public bool IsSuccess => !Errors.Any();
 
+    protected Result() { }
 
-    public Result(T data) => Data = data;
+    // Factory method
+    public static Result Success() => new Result();
 
-    public Result(string error) => ErrorMessage = error;
-    
-    public Result(Exception ex) => Exception = ex;
+    // Factory method
+    // Example: var failureResult = Result.Failure("Error 1", "Error 2", "Error 3");
+    public static Result Failure(params string[] errors)
+    {
+        var result = new Result();
+        result._errors.AddRange(errors);
+        return result;
+    }
 
+    // This will be usefull when the result will have errors from different
+    // places of our system, making it possible to add errors to already created result.
+    // Also used by generic implementation of Result
+    public void AddError(string error) => _errors.Add(error);
 }
 
+public class Result<T> : Result
+{
+    public T Data { get; private set; }
+
+    private Result(T data)
+    {
+        Data = data;
+    }
+
+    // Factory method
+    public static Result<T> Success(T data) => new Result<T>(data);
+
+    // Factory method
+    // Example: var failureResult = Result<T>.Failure("Error 1", "Error 2", "Error 3");
+    public new static Result<T> Failure(params string[] errors)
+    {
+        var result = new Result<T>(default!);
+        foreach (var error in errors)
+        {
+            result.AddError(error);
+        }
+        return result;
+    }
+}
